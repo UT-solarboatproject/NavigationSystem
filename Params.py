a6096b81d7a994e47274e8e0084bb676b9bb457e
DaigoFuru <67820107+DaigoFuru@users.noreply.github.com>
2020-10-13 14:47:05 +0900

    Update Params.py
    INA226を差し込んだことによるPIN number 変更を反映

--- Params.py
+++ Params.py
@@ -12,8 +12,8 @@ class Params:
     def __init__(self):
         # pin assign (GPIO)
         self.pin_mode_in      =  4 # GPIO  4 # PIN  7
-        self.pin_servo_in     =  2 # GPIO  2 # PIN  3
-        self.pin_thruster_in  =  3 # GPIO  3 # PIN  5
+        self.pin_servo_in     =  17 # GPIO  2 # PIN  3
+        self.pin_thruster_in  =  27 # GPIO  3 # PIN  5
         self.pin_servo_out    = 23 # GPIO 23 # PIN 16
         self.pin_thruster_out = 24 # GPIO 24 # PIN 18
 
