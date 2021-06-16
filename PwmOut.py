4ce3a251f8fbbcd5806c457682c39ae754664660
chihaya-kawamura <ckck4253@gmail.com>
2021-06-16 23:45:24 +0900

    改善

--- PwmOut.py
+++ PwmOut.py
@@ -15,10 +15,11 @@ from Params import Params
 
 class PwmOut:
     # [Servo motor]
-    # Stopped     : 1500 microseconds
+    # nuetral servo pulse width = 1500 microseconds
     # [Brushless motor]
-    # Max forward : 1900 microseconds
-    # Max reverse : 1100 microseconds
+    # nuetral thruster pulse width = 1100 microseconds
+    # minmum pulse width= 1100 microseconds
+    # maximum pulse width= 1900 microseconds
 
     frequency = 50
 
@@ -27,14 +28,14 @@ class PwmOut:
         self.pin_servo = pin_servo
         self.pin_thruster = pin_thruster
         self.servo_pulse_width = 1500
-        self.thruster_pulse_width = 1500
+        self.thruster_pulse_width = 1100
 
         # Setup for Out
         self.pi = pigpio.pi()
         self.pi.set_mode(self.pin_servo, pigpio.OUTPUT)
         self.pi.set_mode(self.pin_thruster, pigpio.OUTPUT)
         self.pi.set_servo_pulsewidth(self.pin_servo, 1500)  # neutral
-        self.pi.set_servo_pulsewidth(self.pin_thruster, 1500)  # neutral
+        self.pi.set_servo_pulsewidth(self.pin_thruster, 1100)  # neutral
         return
 
     def end(self):
@@ -54,11 +55,12 @@ if __name__ == "__main__":
     try:
         params = Params()
         sample = PwmOut(params.pin_servo_out, params.pin_thruster_out)
-        # servo
-        servo_pulse_width = 1500
-        # thruster
-        minPulse = 1100
-        maxPulse = 1900
+        # [Servo motor]
+        # nuetral servo pulse width = 1500 microseconds
+        # [Brushless motor]
+        # nuetral thruster pulse width = 1100 microseconds
+        # minmum pulse width= 1100 microseconds
+        # maximum pulse width= 1900 microseconds
 
         print(
             "Initialaze Brushless Motor and Servo Motor. Please reconnect the batteries."
@@ -74,43 +76,31 @@ if __name__ == "__main__":
         print('"j" to down speed')
         print('"k" to turn right')
         print('"h" to turn left')
-        print("speed = %d" % sample.thruster_pulse_width)
+        print(f"speed = {sample.thruster_pulse_width} direction = {sample.servo_pulse_width}")
         while True:
             sample.update_pulse_width()
             inp = input()
             if inp == "u":
                 sample.thruster_pulse_width += 100  # incrementing the speed like hell
-                print(
-                    "speed = %d direction = %d"
-                    % (sample.thruster_pulse_width, sample.servo_pulse_width)
-                )
+                print(f"speed = {sample.thruster_pulse_width} direction = {sample.servo_pulse_width}")
             elif inp == "j":
                 sample.thruster_pulse_width -= 100
-                print(
-                    "speed = %d direction = %d"
-                    % (sample.thruster_pulse_width, sample.servo_pulse_width)
-                )
+                print(f"speed = {sample.thruster_pulse_width} direction = {sample.servo_pulse_width}")
             elif inp == "k":
                 sample.servo_pulse_width += 100
-                print(
-                    "speed = %d direction = %d"
-                    % (sample.thruster_pulse_width, sample.servo_pulse_width)
-                )
+                print(f"speed = {sample.thruster_pulse_width} direction = {sample.servo_pulse_width}")
             elif inp == "h":
                 sample.servo_pulse_width -= 100
-                print(
-                    "speed = %d direction = %d"
-                    % (sample.thruster_pulse_width, sample.servo_pulse_width)
-                )
+                print(f"speed = {sample.thruster_pulse_width} direction = {sample.servo_pulse_width}")
             elif inp == "stop":
                 sample.thruster_pulse_width = 1100
+                sample.servo_pulse_width = 1500
                 break
             else:
                 print("stop or u or j or k or h!")
-        sample.finalize()
         print("Execution Successed.")
     except KeyboardInterrupt:
         print("KeyboardInterrupt")
     finally:
-        sample.finalize()
+        sample.end()
         print("Execution finished.")
