729fe93bc4009165ceca08eefddbc29a33742ddf
kosuke <ksksnd@gmail.com>
2021-06-18 17:22:35 +0900

    fixed shit

--- Driver.py
+++ Driver.py
@@ -41,6 +41,11 @@ class Driver:
         self._params = Params()
         self._status = Status(self._params)
         self._sleep_time = 1
+        self.log_time = time.time()
+        # self.pin_mode = self._params.pin_mode_in
+        # self.pin_servo = self._params.pin_servo
+        # self.pin_thruster = self._params.pin_thruster
+        # self.pin_or = self._params.pin_or
         self._pwm_read = PwmRead(
             self._params.pin_mode_in,
             self._params.pin_servo_in,
@@ -113,16 +118,8 @@ class Driver:
             self._status.read_gps()
 
             self._update_mode()
-
-            # for test
-            self._pwm_read.print_pulse_width()
-
-            # ina226
-            try:
-                self.i_sensor.log()
-            except:
-                pass
-
+                        
+            #action base on mode
             mode = self._status.mode
             if mode == "RC":
                 pass
@@ -131,9 +128,21 @@ class Driver:
             elif mode == "OR":
                 self._out_of_range_operation()
 
+            #update output
             self._pwm_out.update_pulse_width()
-            self._print_log()
-            time.sleep(self._sleep_time)
+            
+            if time.time() - self.log_time > 1:
+                self.log_time = time.time()
+                # for test
+                self._pwm_read.print_pulse_width()
+
+                # ina226
+                try:
+                    self.i_sensor.log()
+                except:
+                    pass
+                self._print_log()
+                # time.sleep(self._sleep_time)
         return
 
     def _update_mode(self):
